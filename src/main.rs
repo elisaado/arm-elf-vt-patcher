d9f4046154457b486d3858e74ffa746c26419a58
Eli Saado <eli5saado@gmail.com>
2025-07-01 18:30:31 +0200

    refactor loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::Parser;
 use clap::ArgAction;
+use clap::Parser;
 use clap_num::maybe_hex;
 use goblin::elf::{Elf, SectionHeader};
 use object::{Architecture, Object, ObjectSymbol};
@@ -26,7 +26,11 @@ struct Args {
     #[arg(short, long, value_parser=maybe_hex::<u64>, help = "Which entry *in the vector table* to write to (defaults to the first zero entry in the interrupts section of the vector table)")]
     n_th_entry: Option<usize>,
 
-    #[arg(short, long, help = "Do not automatically correct interrupt address to account for ARM-thumb mode")]
+    #[arg(
+        short,
+        long,
+        help = "Do not automatically correct interrupt address to account for ARM-thumb mode"
+    )]
     do_not_correct: bool,
 }
 
@@ -55,7 +59,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         // because we manually do +1 for thumb mode
         return Err("ELF is not ARM architecture... Not implemented...".into());
     }
-    
+
     if args.input_file == args.output_file {
         return Err("Cannot use input file as output file!".into());
     }
@@ -72,39 +76,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         section.sh_offset
     );
 
-    let file_offset = section.sh_offset + (symbol_address - section.sh_addr);
-
-    let offset = file_offset as usize;
+    let offset = (section.sh_offset + (symbol_address - section.sh_addr)) as usize;
 
     let mut entry = args.n_th_entry.unwrap_or(0);
     if args.n_th_entry.is_none() {
-        // we start at 16 because that's where the interrupts start
-        for i in (16..) {
-            if i >= section.sh_size {
+        // we start at 16 because that's where the interrupts start in the vector table
+        let found_entry = (16usize..section.sh_size as usize).find(|i| {
+            let word = &buffer[offset + i * 4..offset + i * 4 + 4];
+            let word = u32::from_le_bytes(word.try_into().unwrap());
+
+            word == 0x0
+        });
+
+        entry = match found_entry {
+            Some(i) => i,
+            None => {
                 return Err(Box::from(
                     "No zero-addresses found in interrupt list to overwrite. Try specifying an n-th entry to patch manually",
                 ));
             }
-
-            let i = i as usize;
-            let word = &buffer[offset + i * 4..offset + i * 4 + 4];
-
-            let word = u32::from_le_bytes(word.try_into().unwrap());
-
-            if word == 0x0 {
-                entry = i;
-                break;
-            }
-        }
+        };
     }
-    
+
     let mut interrupt_address = args.interrupt_address;
 
     let interrupt_address_from_sym = obj.symbols().find_map(|sym| {
         let addr = sym.address();
-        if (addr == args.interrupt_address as u64 || addr == (args.interrupt_address + 1) as u64)
-            && sym.size() > 0
-        {
+        if sym.size() == 0 {
+            return None;
+        }
+
+        if (addr == args.interrupt_address as u64 || addr == (args.interrupt_address + 1) as u64) {
             Some(addr as u32)
         } else {
             None
@@ -116,30 +118,28 @@ fn main() -> Result<(), Box<dyn Error>> {
             "Provided interrupt address was not found as a symbol or its symbol size was 0! Proceed with caution!"
         );
     }
-    
+
     if !args.do_not_correct {
-        interrupt_address = interrupt_address_from_sym
-            .unwrap_or(args.interrupt_address);
+        interrupt_address = interrupt_address_from_sym.unwrap_or(args.interrupt_address);
 
         if args.interrupt_address != interrupt_address {
-            println!("Automatically corrected interrupt address to account for ARM-thumb mode. Pass -d to disable correction.");
+            println!(
+                "Automatically corrected interrupt address to account for ARM-thumb mode. Pass -d to disable correction."
+            );
         }
     }
-    
-    println!(
-        "Using interrupt address 0x{:x}",
-        (interrupt_address)
-    );
-    
 
-    buffer[offset + entry * 4..offset + entry * 4 + 4].copy_from_slice(&interrupt_address.to_le_bytes());
+    println!("Using interrupt address 0x{:x}", (interrupt_address));
+
+    buffer[offset + entry * 4..offset + entry * 4 + 4]
+        .copy_from_slice(&interrupt_address.to_le_bytes());
 
     if let Err(err) = write_file(new_path, &buffer) {
         println!("Failed to write output file: {err}");
         return Err(err.into());
     }
 
-    println!("Patched bytes at file offset 0x{file_offset:x}");
+    println!("Patched bytes at file offset 0x{offset:x}");
 
     Ok(())
 }
